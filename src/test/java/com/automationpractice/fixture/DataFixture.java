8e0d0f14cd2363748f9872c6271c1c2e16961093
shabalin_siarhei <sergeimkmouse@gmail.com>
2020-02-29 16:16:45 +0300

    Commit

--- src/test/java/com/automationpractice/fixture/DataFixture.java
+++ src/test/java/com/automationpractice/fixture/DataFixture.java
@@ -29,8 +29,8 @@ public class DataFixture {
 //        Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = startPageUrl;
         Configuration.remote = "http://localhost:4444/wd/hub/";
-//        Configuration.browser = "chrome";
-        Configuration.browser = "internet explorer";
+        Configuration.browser = "chrome";
+//        Configuration.browser = "internet explorer";
         Configuration.browserSize = "1920x1080";
 //        Configuration.browserVersion = "80.0.3987.106";
 //        DesiredCapabilities capabilities = new DesiredCapabilities();
