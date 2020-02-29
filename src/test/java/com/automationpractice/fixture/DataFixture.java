fc28cce56be237b5513f1ea450f99dc7252e95bc
shabalin_siarhei <sergeimkmouse@gmail.com>
2020-02-29 13:48:45 +0300

    Commit

--- src/test/java/com/automationpractice/fixture/DataFixture.java
+++ src/test/java/com/automationpractice/fixture/DataFixture.java
@@ -7,13 +7,19 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
 
 
 public class DataFixture {
@@ -29,20 +35,19 @@ public class DataFixture {
     @BeforeEach
     public void beforeTest() throws MalformedURLException, MalformedURLException {
 //        Configuration.holdBrowserOpen = true;
-//        Configuration.baseUrl = startPageUrl;
+        Configuration.baseUrl = startPageUrl;
         Configuration.remote = "http://localhost:4444/wd/hub/";
-//        Configuration.browserSize = "1920х1080";
         Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
 //        Configuration.browserVersion = "80.0.3987.106";
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName("chrome");
-        capabilities.setVersion("80.0.3987.106");
-        capabilities.setPlatform(Platform.ANY);
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setBrowserName("chrome");
+//        capabilities.setVersion("80.0.3987.106");
+//        capabilities.setPlatform(Platform.ANY);
 //        capabilities.setCapability("enableVNC", true);
 //        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
-//        WebDriverRunner.setWebDriver(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities));
-        open(startPageUrl);
+//        Configuration.browserCapabilities = capabilities;
+        open("");
     }
 
     @AfterEach
