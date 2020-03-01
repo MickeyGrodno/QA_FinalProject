a61684b99a040f8c25215065dad7eeaee16fab25
shabalin_siarhei <sergeimkmouse@gmail.com>
2020-03-01 18:57:56 +0300

    Commit

--- src/test/java/com/automationpractice/fixture/DataFixture.java
+++ src/test/java/com/automationpractice/fixture/DataFixture.java
@@ -5,9 +5,13 @@ import com.automationpractice.pageobject.MyAccountPage;
 import com.automationpractice.pageobject.ShoppingCartPage;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.SessionId;
+
+import java.net.MalformedURLException;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -20,7 +24,6 @@ public class DataFixture {
     protected MyAccountPage myAccountPage = new MyAccountPage();
     protected ShoppingCartPage shoppingCartPage = new ShoppingCartPage();
 
-
     @BeforeEach
     public void beforeTest() {
 
@@ -30,14 +33,22 @@ public class DataFixture {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setAcceptInsecureCerts(false);
         capabilities.setCapability("enableVideo", true);
+//        capabilities.setCapability("enableVNC", true);
         Configuration.browserCapabilities = capabilities;
         Configuration.remote = "http://localhost:4444/wd/hub/";
 
         open(startPageUrl);
     }
 
+    @Attachment(value = "Video HTML", type = "text/html", fileExtension = ".html")
+    public static String videoInHtml(String sessionId) {
+        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
+                + "http://127.0.0.1:4444/video/" + sessionId + ".mp4"
+                +"' type='video/mp4'></video></body></html>";
+    }
+
     @AfterEach
-    public void afterTest(){
+    public void afterTest() {
         WebDriverRunner.closeWindow();
     }
 }
